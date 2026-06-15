20d46f0688547509d55ac405dc58b607562383cf
BitRazor <arnoldas.ltu@gmail.com>
2026-06-15 19:38:27 +0200

    Update week 1 (2026-06-15 17:38)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // offline cache for the meal app (precache shell, cache-first)
-const C="meal-week-1-9b6acbe7";
+const C="meal-week-1-c2f38b6f";
 const A=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(A);}).then(function(){return self.skipWaiting();}));});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){if(k!==C)return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
